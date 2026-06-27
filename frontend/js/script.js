7419819d9116fbd73117b75d262749948e2fd7d8
Born-as-Harsha <harshamass2007@gmail.com>
2026-06-27 10:47:04 +0530

    Created frontend landing page using HTML CSS and JavaScript

---
+++ frontend/js/script.js
@@ -0,0 +1,13 @@
+console.log("Enterprise AI-Powered Cloud ERP Suite Started");
+
+const projectName = "Enterprise AI-Powered Cloud ERP Suite";
+
+console.log(projectName);
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+
+    alert("Welcome to the Enterprise AI-Powered Cloud ERP Suite!");
+
+});
